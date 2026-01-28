2a9cb039e0ce98ead1be6992d539bfc1e5efe3ca
IBSpark <ibadweb68@gmail.com>
2026-01-28 13:07:25 +0500

    add a new navbar icon

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,7 @@ export default function Home() {
    {/* Header */}
       <header className="top-bar">
         <div className="ai-logo">
-          <img src={`${process.env.PUBLIC_URL}/images/icon.png`} alt="AI Icon"className="ai-icon-img"/>
+          <img src={`${process.env.PUBLIC_URL}/images/icon.svg`} alt="AI Icon"className="ai-icon-img"/>
           <span className="ai-text">AI Text to Voice</span>
         </div>
 
